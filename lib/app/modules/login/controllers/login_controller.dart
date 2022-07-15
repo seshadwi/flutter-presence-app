9aef2ea6115dc51211ad6d18a5e943be00fe6624
seshadwi <seshaadwi@gamil.com>
2022-07-15 09:45:33 +0700

    add auth login

---
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -0,0 +1,46 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:presence_app/app/routes/app_pages.dart';
+
+class LoginController extends GetxController {
+  TextEditingController emailC = TextEditingController();
+  TextEditingController passwordC = TextEditingController();
+
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  void login() async {
+    if (emailC.text.isNotEmpty && passwordC.text.isNotEmpty) {
+      try {
+        UserCredential userCredential = await auth.signInWithEmailAndPassword(
+          email: emailC.text,
+          password: passwordC.text,
+        );
+
+        print(userCredential);
+
+        if (userCredential.user != null) {
+          if (userCredential.user!.emailVerified == true) {
+            Get.offAllNamed(Routes.HOME);
+          } else {
+            Get.defaultDialog(
+              title: "Belum Verifikasi",
+              middleText:
+                  "Akun ini belum diverifikasi. Lakukan verifikasi terlebih dahulu diemail.",
+            );
+          }
+        }
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'user-not-found') {
+          Get.snackbar("Terjadi kesalahan", "No user found for that email.");
+        } else if (e.code == 'wrong-password') {
+          Get.snackbar("Error", "Wrong password provided for that user.");
+        }
+      } catch (e) {
+        Get.snackbar("Error", "Tidak dapat login.");
+      }
+    } else {
+      Get.snackbar("Terjadi kesalahan", "Email dan Password harus diisi.");
+    }
+  }
+}
