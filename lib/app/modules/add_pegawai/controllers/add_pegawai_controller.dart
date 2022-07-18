e9b3581361db6df9d0d09938aacfd20aa29cbe82
seshadwi <seshaadwi@gamil.com>
2022-07-18 22:50:24 +0700

    integrate auto login

--- lib/app/modules/add_pegawai/controllers/add_pegawai_controller.dart
+++ lib/app/modules/add_pegawai/controllers/add_pegawai_controller.dart
@@ -7,23 +7,30 @@ class AddPegawaiController extends GetxController {
   TextEditingController nameC = TextEditingController();
   TextEditingController nipC = TextEditingController();
   TextEditingController emailC = TextEditingController();
+  TextEditingController passAdminC = TextEditingController();
 
   FirebaseAuth auth = FirebaseAuth.instance;
   FirebaseFirestore firestore = FirebaseFirestore.instance;
 
-  void addPegawai() async {
-    if (nameC.text.isNotEmpty &&
-        nipC.text.isNotEmpty &&
-        emailC.text.isNotEmpty) {
+  Future<void> addPegawaiProcess() async {
+    if (passAdminC.text.isNotEmpty) {
       try {
-        UserCredential userCredential =
+        String emailAdmin = auth.currentUser!.email!;
+
+        UserCredential userCredentialAdmin =
+            await auth.signInWithEmailAndPassword(
+          email: emailAdmin,
+          password: passAdminC.text,
+        );
+
+        UserCredential pegawaiCredential =
             await auth.createUserWithEmailAndPassword(
           email: emailC.text,
           password: "password",
         );
 
-        if (userCredential.user != null) {
-          String uid = userCredential.user!.uid;
+        if (pegawaiCredential.user != null) {
+          String uid = pegawaiCredential.user!.uid;
 
           await firestore.collection("pegawai").doc(uid).set({
             "nip": nipC.text,
@@ -33,19 +40,74 @@ class AddPegawaiController extends GetxController {
             "createdAt": DateTime.now().toIso8601String(),
           });
 
-          await userCredential.user!.sendEmailVerification();
-        }
+          await pegawaiCredential.user!.sendEmailVerification();
 
-        print(userCredential);
+          await auth.signOut();
+
+          UserCredential userCredentialAdmin =
+              await auth.signInWithEmailAndPassword(
+            email: emailAdmin,
+            password: passAdminC.text,
+          );
+
+          Get.back(); // close dialog
+          Get.back(); // back to home
+          Get.snackbar("Berhasil", "Berhasil menambahkan pegawai");
+          // await auth.signInWithEmailAndPassword(email: email, password: password);
+        }
       } on FirebaseAuthException catch (e) {
         if (e.code == 'weak-password') {
           Get.snackbar("Terjadi kesalahan", "Password yang digunakan lemah.");
         } else if (e.code == 'email-already-in-use') {
           Get.snackbar("Terjadi kesalahan", "Akun telah digunakan.");
+        } else if (e.code == 'wrong-password') {
+          Get.snackbar("Terjadi kesalahan", "Password salah.");
+        } else {
+          Get.snackbar("Terjadi kesalahan", "${e.code}");
         }
       } catch (e) {
         Get.snackbar("Terjadi Kesalahan", "Tidak dapat menambahkan akun.");
       }
+    } else {
+      Get.snackbar(
+          "Terjadi kesalahan", "Password wajib diisi untuk keperluan validasi");
+    }
+  }
+
+  void addPegawai() async {
+    if (nameC.text.isNotEmpty &&
+        nipC.text.isNotEmpty &&
+        emailC.text.isNotEmpty) {
+      Get.defaultDialog(
+        title: "Validasi Admin",
+        content: Column(
+          children: [
+            Text("Masukkan password untuk validasi admin."),
+            TextField(
+              controller: passAdminC,
+              autocorrect: false,
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: "Password",
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ],
+        ),
+        actions: [
+          OutlinedButton(
+            onPressed: () => Get.back(),
+            child: Text("Cancel"),
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              await addPegawaiProcess();
+            },
+            child: Text("Add Pegawai"),
+          ),
+        ],
+      );
+
 // excute storage
     } else {
       Get.snackbar("Terjadi Kesalahan", "NIP, Nama, Email tidak boleh kosong.");
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// import 'dart:js';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -12,10 +14,25 @@ void main() async {
   );
 
   runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: Routes.LOGIN,
-      getPages: AppPages.routes,
+    StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return MaterialApp(
+            home: Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+          );
+        }
+        print(snapshot.data);
+        return GetMaterialApp(
+          title: "Application",
+          initialRoute: snapshot.data != null ? Routes.HOME : Routes.LOGIN,
+          getPages: AppPages.routes,
+        );
+      },
     ),
   );
 }
